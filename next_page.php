d604ec9b497bb2c9d24880d9a2ae5168754e86f5
nurnabilahhh <nabilahshaari01@gmail.com>
2022-05-25 17:21:36 +0800

    3rd commit

--- next_page.php
+++ next_page.php
@@ -11,11 +11,23 @@
     session_start();
     echo "<html>";
 
-    $myLogin = $_SESSION["myLogin"];
-    echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+    $queryString = $_SERVER["QUERY_STRING"];
+    echo "Query string of the incoming URL: ".$queryString."\n";
 
+    echo "Cookies received:\n";
+    foreach ($_COOKIES as $name => $value){
+        echo " $name = $value\n";
+    }
+
+    $sid = session_id();
+    echo "Session ID returned by session_id(): ".$sid."<br>";
+    $sid = SID;
+    echo "Session ID returned by SID: ".$sid."<br>";
+
+    $myLogin = $_SESSION["myLogin"];
+    echo "Value of myLogin has been retrieved: ".$myLogin."<br>";
     $myColor = $_SESSION["myColor"];
-    echo "Value of myColor has been retrieved: ".$myColor."\n";
+    echo "Value of myColor has been retrieved: ".$myColor."<br>";
 
     echo "</html>\n";
     ?>
